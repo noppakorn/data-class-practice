5d3e485e5fe36c17b355e60db517061ddc69fd1c
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-07-16 22:00:07 +0700

    Add working CP::deque

--- src/cpdequetest.cpp
+++ src/cpdequetest.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-//#include "stl-cp/deque.h"
+#include "stl-cp/deque.h"
 
 using namespace std;
 
+void printLine() {
+    cout << "-----" << endl;
+}
 int main() {
-    cout << "Hello world" << endl;
+    CP::deque<int> q;
+    cout << "Push back 1,2" << endl;
+    q.push_back(1);
+    q.push_back(2);
+    while (!q.empty()) {
+        cout << q.front() << endl;
+        q.pop_front();
+    }
+
+    printLine();
+
+    cout << "Push back 1,2 Push front 0, Push Back 200,201" << endl;
+    q.push_back(1);
+    q.push_back(2);
+    q.push_front(0);
+    q.push_back(200);
+    q.push_back(201);
+    cout << "After pop 1: " << q.back() << endl;
+    q.pop_back();
+    cout << "After pop 2: " << q.back() << endl;
+    q.pop_back();
+    cout << "After pop 3: " << q.back() << endl;
+    while (!q.empty()) {
+        cout << q.front() << endl;
+        q.pop_front();
+    }
+
+    printLine();
+
+    cout << "Push front starting from 10 to 1" << endl;
+    for (int i = 10; i > 0; i--) {
+        q.push_front(i);
+    }
+    while (!q.empty()) {
+        cout << q.front() << endl;
+        q.pop_front();
+    }
 }
--- src/stl-cp/deque.h
+++ src/stl-cp/deque.h
@@ -1,6 +1,78 @@
 namespace CP {
     template <typename T>
     class deque {
-        deque() {}
+        protected:
+            T *mData;
+            size_t mSize;
+            size_t mCap;
+            size_t mFront;
+            void expand(size_t capacity) {
+                T *arr = new T[capacity]();
+                for (int i = 0; i < mSize; i++) {
+                    arr[i] = mData[(mFront + i) % mCap];
+                }
+                delete [] mData;
+                mData = arr;
+                mCap = capacity;
+                mFront = 0;
+            }
+            void ensureCapacity(size_t capacity) {
+                if (capacity > mCap) {
+                    size_t s = (capacity > 2 * mCap) ? capacity : 2 * capacity;
+                    expand(s);
+                }
+            }
+        public:
+            deque() : mData(new T[mCap]()), mCap(1), mSize(0), mFront(0) {}
+            deque(const deque<T> &a) : mData(new T[a.mCap]()), mCap(a.mCap), mSize(a.mSize), mFront(a.mFront) {
+                for (int i = 0; i < mCap; i++) {
+                    mData[i] = a.mData[i];
+                }
+            }
+            deque<T>& operator=(deque<T> other) {
+                using std::swap;
+                swap(mSize, other.mSize);
+                swap(mCap, other.mCap);
+                swap(mFront, other.mFront);
+                swap(mData, other.mData);
+                return *this;
+            }
+            ~deque() {
+                delete [] mData;
+            }
+            // Capacity Function
+            bool empty() const {
+                return mSize == 0;
+            }
+            bool size() const {
+                return mSize;
+            }
+            // Access 
+            const T& front() const {
+                return mData[mFront];
+            }
+            const T& back() const {
+                return mData[(mFront + mSize - 1) % mCap];
+            }
+            // Modifier
+            void push_front(const T &element) {
+                ensureCapacity(mSize+1);
+                mFront = (mFront + mCap - 1) % mCap;
+                mData[mFront] = element;
+                mSize++;
+            }
+            void push_back(const T &element) {
+                ensureCapacity(mSize+1);
+                mData[(mFront + mSize) % mCap] = element;
+                mSize++;
+            }
+            void pop_front() {
+                mFront = (mFront + 1) % mCap;
+                mSize--;
+            }
+            void pop_back() {
+                mSize--;
+            }
+            
     };
 }
